507854e24ac780f0be16e8bd9f0ac20431921e10
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-21 11:54:14 +0200

    fini

---
+++ main.js
@@ -0,0 +1,11 @@
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+eleves.forEach(element => {
+    console.log("Bonjour " + element);
+});
+
+let nbre = +prompt("Entrez un nombre");
+
+for (i=0;i<nbre;i++) {
+    console.log(i);
+};
